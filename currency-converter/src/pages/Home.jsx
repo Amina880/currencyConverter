50bc85e2e34e6fabe6c5352f4a28b4eb5c5e54aa
Amina880 <dkalekwa37@gmail.com>
2024-11-07 21:34:23 +0300

    chore: created home page

---
+++ currency-converter/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import CurrencySelector from '../components/currencySelector'
+import Layout from '../components/Layout'
+
+
+function Home() {
+
+  return (
+    <>
+    {/*Home Page */}
+    <div className='min-h-screen flex flex-col bg-gradient-to-r from-blue-700 to-blue-900'>
+        <Layout>
+        <div className='mx-auto mt-6 sm:mt-12 mb-2 w-fit font-medium text-center sm:text-2xl text-lg text-white pb-4 sm:pb-8 '>
+            {/*Hero Piece*/}
+            <p>Global Currency Conversions You Can Trust</p>
+        </div>
+        <CurrencySelector />
+        </Layout>
+    </div> 
+    </>
+  )
+}
+
+export default Home
