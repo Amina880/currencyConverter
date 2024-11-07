1192d2f6dfd7bc8dc1211b7355ef01a79d84c4ce
Amina880 <dkalekwa37@gmail.com>
2024-11-08 00:49:21 +0300

    styles: change dark mode toggle width

--- currency-converter/src/components/Nav.jsx
+++ currency-converter/src/components/Nav.jsx
@@ -27,16 +27,16 @@ function Nav() {
                 <Link to="/">
                 <img className='w-20' src="/images/currency-converter-favicon-color.png" alt='logo'/>
                 </Link>
-                <div className='flex justify-evenly w-1/3'>
+                <div className='flex justify-evenly w-1/2'>
                     <button onClick={toggleNav}>
                         {isOpen ? <X color='white' className='w-20' /> : <Menu color='white' className='w-20 '/>}
                     </button>
-                    <DarkModeToggle />
+                    <DarkModeToggle className='w-20' />
                 </div>
             </div>
             {/*Mobile Links */}
             {isOpen && (
-                <div className='flex flex-col items-center justify-center w-full left-0 bg-white space-y-4 h-screen underline underline-offset-4 decoration-1 decoration-slate-400'>
+                <div className='flex flex-col items-center justify-center w-full left-0 bg-white dark:bg-slate-400 dark:text-slate-100 space-y-4 h-screen underline underline-offset-4 decoration-1 decoration-slate-400 dark:decoration-white'>
                 <Link to="/">Home</Link>
                 <Link to="/favourites">Favourites</Link>
                 </div>
