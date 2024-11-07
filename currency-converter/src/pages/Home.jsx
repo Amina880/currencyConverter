1240c6d8407fd8d5a281eeaa94de8f2a6428e916
Amina880 <dkalekwa37@gmail.com>
2024-11-07 22:37:44 +0300

    styles: added px on header

--- currency-converter/src/pages/Home.jsx
+++ currency-converter/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     {/*Home Page */}
     <div className='min-h-screen flex flex-col bg-gradient-to-r from-blue-700 to-blue-900'>
         <Layout>
-        <div className='mx-auto mt-6 sm:mt-12 mb-2 w-fit font-medium text-center sm:text-2xl text-lg text-white pb-4 sm:pb-8 '>
+        <div className='mx-auto px-4 mt-6 sm:mt-12 mb-2 w-fit font-medium text-center sm:text-2xl text-lg text-white pb-4 sm:pb-8 '>
             {/*Hero Piece*/}
             <p>Global Currency Conversions You Can Trust</p>
         </div>
